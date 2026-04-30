27b6c957b7bd1b15d79ba69afc07e0ad9aa65f84
Ganesh <ganeshgarje716@gmail.com>
2026-04-30 19:51:33 +0530

    In Student Class Create Veriables, Create Constructor, Create Getter Setter Methods and Create toString Method

--- HibernateStudent/src/main/java/com/Dao/StudentDao.java
+++ HibernateStudent/src/main/java/com/Dao/StudentDao.java
@@ -1,5 +1,6 @@
 package com.Dao;
 
 public class StudentDao {
+	 
 
 }
--- HibernateStudent/src/main/java/com/entity/Student.java
+++ HibernateStudent/src/main/java/com/entity/Student.java
@@ -1,15 +1,141 @@
 package com.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Student {
 	
 	
+	@Id
 	private int id;
 	
 	private String name;
 	
 	private String email;
 	
-	private 
+	private float mobileNo;
+	
+	private String address;
+	
+	private String branch;
+	
+	private double cgpa;
+
+	
+	
+	public Student(int id, String name, String email, float mobileNo, String address, String branch, double cgpa) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.mobileNo = mobileNo;
+		this.address = address;
+		this.branch = branch;
+		this.cgpa = cgpa;
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public float getMobileNo() {
+		return mobileNo;
+	}
+
+
+
+	public void setMobileNo(float mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public String getBranch() {
+		return branch;
+	}
+
+
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+
+
+	public double getCgpa() {
+		return cgpa;
+	}
+
+
+
+	public void setCgpa(double cgpa) {
+		this.cgpa = cgpa;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", email=" + email + ", mobileNo=" + mobileNo + ", address="
+				+ address + ", branch=" + branch + ", cgpa=" + cgpa + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
